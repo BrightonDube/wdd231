36828466e4f777e5f0214bee2e15d9bfcaff13ae
Brighton <BrightonDube@users.noreply.github.com>
2025-03-25 18:21:51 +0200

    `Added membership application form to join.html`

---
+++ chamber/scripts/join.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const timestampInput = document.getElementById('timestamp');
+    if (timestampInput) {
+      timestampInput.value = new Date().toISOString(); 
+    }
+
+    const dialog = document.getElementById('contact-form');
+    const openButton = document.getElementById('openButton');
+
+    openButton.addEventListener('click', () => {
+      dialog.showModal();
+    });
+
+    const form = document.getElementById('join-form');
+    form.addEventListener('submit', (event) => {
+      
+    });
+
+
+  });
